84214f8137fbb84aa45d775f959c11b5821ad89e
Tinaes <aj.tinaes@gmail.com>
2021-08-24 18:07:12 +0800

    someVariable

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-console.log("HELLO!");
-
-
 var main = function (input) {
-  console.log("GOODBYE!");
-  var myOutputValue = input * 5;
-  return myOutputValue;
-};
+
+// declare a variable and assign it the numerical value of 'input'
+var someVariable = Number(input)
+console.log(`You entered ${input}`);
+
+// increment the value of someVariable by 1.
+someVariable = someVariable + 1;
+console.log(`${input} + 1 is ${someVariable}`);
+
+// double the value of someVariable
+someVariable = someVariable * 2;
+console.log(`(${input} + 1) * 2 is ${someVariable}`);
+
+// returns someVariable, which is effectively (input + 1) * 2
+return someVariable;
+}
